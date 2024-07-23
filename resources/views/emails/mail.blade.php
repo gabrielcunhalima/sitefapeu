0fed87a0a66fcd2eeabb624373e892cb0c728bb1
Gabriel Lima <gaculi@hotmail.com>
2024-07-23 14:28:34 -0300

    x

--- resources/views/emails/mail.blade.php
+++
@@ -1,6 +0,0 @@
-{{$nomeCompleto}},
-
-Sua inscrição (Nº {{$idInscricao}}) no evento {{$nomeEvento}} foi confirmada com sucesso.
-
-**Mensagem automática, favor não responder, caso de dúvidas entre em contato com o evento**
-
--- resources/views/errors/404.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.header')
-@section('title','[FAPEU] Inscrição em eventos')
-
-@section('conteudo')
-<center>
-    <h1>  <i class="material-icons">warning</i> <br>404</h1>
-</center>
-
-@endsection
