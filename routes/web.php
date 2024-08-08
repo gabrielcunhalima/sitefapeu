bebd8e68e6973d204c880c0c7a51abfedb279ee2
Gabriel Lima <gaculi@hotmail.com>
2024-08-08 15:33:31 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\HomeController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
-});
+// Route::get('/home', function () {
+//     return view('home');
+// });
 
-Route::get('/login', function () {
-    return view('login');
-});
+// Route::get('/login', function () {
+//     return view('login');
+// });
+
+Route::get('/home',[HomeController::class,'index'])->name('homepage.home');
