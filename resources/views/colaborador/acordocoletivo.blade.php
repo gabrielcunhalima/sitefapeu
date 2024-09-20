3138fea53579c3d6de34b18e25cc6c951ea830ff
Juliana <juhorizont@gmail.com>
2024-09-20 15:13:22 -0300

    commit 2 formularios

--- resources/views/colaborador/acordocoletivo.blade.php
+++ resources/views/colaborador/acordocoletivo.blade.php
@@ -3,4 +3,43 @@
 
 @section('conteudo')
 
+
+
+
+<style>
+  .table {
+    width: 80%; /* Ajuste a porcentagem conforme necessário */
+    margin: 0 auto; /* Centraliza a tabela */
+  }
+
+</style>
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Formulário</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><a href="/pdfs/atualizacao_cadastro_empregado (2).doc" download>Acordo Coletivo 2023/2025  // ADITIVO</a></td>
+
+    </tr>
+    <tr>
+      <td><a href="/pdfs/formulario_motivo_falta.doc" download>Acordo Coletivo 2022/23</a></td>
+     
+    </tr>
+    <tr>
+      <td><a href="/pdfs/solicitacao_cracha.doc" download>Acordo Coletivo 2021/22</a></td>
+ 
+    </tr>
+
+    <tr>
+      <td><a href="/pdfs/solicitacao_vale_transporte.doc" download>	Acordo Coletivo 2020/21</a></td>
+
+ 
+
+  </tbody>
+</table>
 @endsection
--- resources/views/colaborador/formularioscolaborador.blade.php
+++ resources/views/colaborador/formularioscolaborador.blade.php
@@ -9,8 +9,11 @@
 /* Define uma largura fixa para a coluna "Última Atualização" */
   .table th:nth-child(2), 
   .table td:nth-child(2) {
-    width: 200px; /* Define uma largura específica para a coluna */
+    width: 350px; /* Define uma largura específica para a coluna */
   }
+
+  
+
 </style>
 
 <table class="table">
@@ -22,17 +25,28 @@
   </thead>
   <tbody>
     <tr>
-      <td><a href="/pdfs/formulario1.pdf" download>Atualização Cadastral do Empregado</a></td>
-      <td><input type="date" name="ultima_atualizacao_1"></td>
+      <td><a href="/pdfs/atualizacao_cadastro_empregado (2).doc" download>Atualização Cadastral do Empregado</a></td>
+      <td><input type="date" name="ultima_atualizacao_1"value="2011-12-15" readonly ></td>
     </tr>
     <tr>
-      <td><a href="/pdfs/formulario2.pdf" download>Baixar Formulário 2</a></td>
-      <td><input type="date" name="ultima_atualizacao_2"></td>
+      <td><a href="/pdfs/formulario_motivo_falta.doc" download>Motivo Falta ao Trabalho</a></td>
+      <td><input type="date" name="ultima_atualizacao_2"value="2015-03-30" readonly ></td>
     </tr>
     <tr>
-      <td><a href="/pdfs/formulario3.pdf" download>Baixar Formulário 3</a></td>
-      <td><input type="date" name="ultima_atualizacao_3"></td>
+      <td><a href="/pdfs/solicitacao_cracha.doc" download>Solicitação de Crachá</a></td>
+      <td><input type="date" name="ultima_atualizacao_3" value="2010-07-22" readonly ></td>
     </tr>
+
+    <tr>
+      <td><a href="/pdfs/solicitacao_vale_transporte.doc" download>	Solicitação de Vale Transporte</a></td>
+      <td><input type="date" name="ultima_atualizacao_4" value="2024-04-16" readonly></td>
+    </tr>
+    <tr>
+      <td><a href="/pdfs/formulario_alteracao_aux_alimentacao.doc" download>	Alteração de Vale Alimentação</a></td>
+      <td><input type="date" name="ultima_atualizacao_5" value="2013-08-16" readonly></td>
+    </tr>
+    <tr>
+
   </tbody>
 </table>
 
