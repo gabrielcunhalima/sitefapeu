67668e2ee635f49b2ba4e98077e72e8be5b89848
Juliana <juhorizont@gmail.com>
2024-09-26 09:02:33 -0300

    commit 26/09

--- resources/views/colaborador/programainclusao.blade.php
+++ resources/views/colaborador/programainclusao.blade.php
@@ -4,9 +4,11 @@
 @section('conteudo')
 
 
+
+
 <div class="jumbotron">
-  <br><h1 class="display-4">Programa Fapeu de Inclusão</h1>
-  <br><p class="lead">O mercado de trabalho está cada dia mais competitivo, exigindo experiências e qualificação de seus profissionais. A FAPEU, por meio do programa de inclusão, quer proporcionar essas experiências e qualificar essas pessoas para desenvolverem seu trabalho.
+  <br><h1 class="display-4 text-center">Programa Fapeu de Inclusão</h1>
+  <br><p class="lead text-center">O mercado de trabalho está cada dia mais competitivo, exigindo experiências e qualificação de seus profissionais. A FAPEU, por meio do programa de inclusão, quer proporcionar essas experiências e qualificar essas pessoas para desenvolverem seu trabalho.
 A FAPEU acredita que a inclusão social é um dever de todos, e é por isso que está disponibilizando vagas para Pessoas com Deficiência. O acesso ao trabalho deverá estar acessível a todos.
 
 <br><br>Valorizamos as diferenças e acreditamos sempre no desenvolvimento humano. Por meio deste programa, conheceremos e aprenderemos mais, teremos contato com novas experiências de vida.
@@ -14,7 +16,8 @@ A FAPEU acredita que a inclusão social é um dever de todos, e é por isso que 
 Venha juntar-se a família FAPEU e fazer a diferença na vida de nossos parceiros.</p> 
   <hr class="my-4">
   <p>Para realizar seu cadastro, clique aqui.</p>
-  <a class="btn btn-primary btn-lg" href="http://150.162.78.45:8080/Curriculo/" target="_blank" role="button">Cadastro</a>
+   <a class="btn btn-lg" href="http://150.162.78.45:8080/Curriculo/" target="_blank" role="button" style="color: white; background-color: #146551;">Cadastro</a>
+    </div>
 </div>
 
 @endsection
