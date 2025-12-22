e6f626b4ea1ea7bfb37abd9b1528c55558a7fe1c
juliana <juhorizont@gmail.com>
2025-12-22 12:00:45 -0300

    heyzine link novo - codigoconduta.blade.php

--- resources/views/politica/codigoconduta.blade.php
+++ resources/views/politica/codigoconduta.blade.php
@@ -7,7 +7,7 @@
         O <b>Código de Conduta da FAPEU</b> permite, além de oferecer a difusão da cultura de integridade, destacar os princípios pelos quais a Fundação conduz as suas relações com todas as partes interessadas, sejam elas pessoas físicas ou jurídicas. Tem por finalidade viabilizar um comportamento ético pautado em valores que devem ser incorporados por seus integrantes e fortalecer a imagem da fundação e daqueles que a compõem junto à sociedade. Deve ser cumprido por todos os profissionais internos e externos, quando estiverem representando os interesses da FAPEU.
     </p>
     <p class="text-start mb-4">
-        <a href="https://heyzine.com/flip-book/5448fcfe82.html" download class="fapeu-btn">
+        <a href="https://heyzine.com/flip-book/d626597d4c.html" download class="fapeu-btn">
             <i class="bi bi-download me-2"></i>Baixar o documento
         </a>
     </p>
